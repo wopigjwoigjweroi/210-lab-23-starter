2b8e76caaab3af8dee27164c8d201384679c4132
wopigjwoigjweroi <agizekel@gmail.com>
2024-10-27 00:32:57 -0700

    Commit 2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib> 
 #include <fstream>
 #include <iomanip>
 #include <list>
@@ -12,6 +13,34 @@ void delete_goat(list<Goat> &trip);
 void add_goat(list<Goat> &trip, string [], string []);
 void display_trip(list<Goat> trip);
 int main_menu();
+
+void add_goat(list<Goat> & trip, string name[], string color[]) {
+
+    int names = rand() % SZ_NAMES;
+
+    int colors = rand() % SZ_COLORS;
+
+    int ages = rand() % MAX_AGE; 
+
+    Goat goat(name[names], ages, color[colors]); 
+
+    trip.push_back(goat); 
+}
+
+int main_menu() {
+
+    int select; 
+
+    cout << "**GOAT MANAGER 3001** \n" << "[1] Add a goat \n" << "[2] Delete a goat \n" << "[3] List goats \n" << "[4] Quit \n" << "Choice ->"; 
+    cin >> select; 
+
+    while (select < 1 || select > 4) {
+
+        cin >> select; 
+    }
+
+    return select; 
+}
 
 int main() {
     srand(time(0));
